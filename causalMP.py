7f47925690b2f97601e9894bb2787a25cdfd7a9b
Eric Dodds <edodds828@gmail.com>
2018-10-23 13:53:24 -0700

    normed thresh added

--- causalMP.py
+++ causalMP.py
@@ -8,9 +8,16 @@ dtype = csn.dtype
 
 class CausalMP(csn.ConvSparseNet):
 
-    def __init__(self, **kwargs):
+    def __init__(self, normed_thresh=None,
+                 mask_epsilon=None, **kwargs):
         csn.ConvSparseNet.__init__(self, **kwargs)
         self.thresh = self.lam
+        if normed_thresh is None:
+            self.normed_thresh = 2/np.sqrt(self.kernel_size)
+        else:
+            self.normed_thresh = normed_thresh
+        self.mask_epsilon = mask_epsilon or 0.01*np.sqrt(1/self.kernel_size)
+        self.masks = self.get_masks()
 
     def infer(self, signal):
         with torch.no_grad():
@@ -40,9 +47,15 @@ class CausalMP(csn.ConvSparseNet):
             dots = torch.mm(segment, torch.t(weights))
             candidates = torch.argmax(torch.abs(dots), dim=1)
             spikes = dots[torch.arange(batch_size), candidates]
-            # segnorm = torch.norm(segment[self.masks[candidates]])
-            spikes = (torch.abs(spikes) > self.thresh).float()*spikes
-            acts[torch.arange(batch_size), candidates, tt] += spikes
+            abspikes = torch.abs(spikes)
+
+            indexer = torch.arange(batch_size)
+            segnorms = torch.norm(segment*self.masks[candidates])
+            norm_condition = (abspikes/segnorms > self.normed_thresh).float()
+            raw_condition = (abspikes > self.thresh).float()
+
+            spikes = raw_condition*norm_condition*spikes
+            acts[indexer, candidates, tt] += spikes
             resid[:, tt:tt+self.kernel_size] -= \
                 spikes[:, None]*weights[candidates, :]
 
@@ -60,3 +73,20 @@ class CausalMP(csn.ConvSparseNet):
                                            device=self.device)],
                       dim=2)
         return torch.mean((padded_signal-recon)**2)
+
+    def extra_updates(self, acts, meta):
+        self.masks = self.get_masks()
+
+    def get_masks(self):
+        """Returns an array where each row is a binary mask for a filter. The
+        mask should be zero outside the region where the filter has significant
+        support. Used for ignoring part of signal segments when assessing
+        whether a candidate coefficient clears the normed_thresh.
+        For now this method assumes that the correct mask has zeros to the left
+        and ones to the right of some point. This is consistent with filters
+        learned in early experiments."""
+        masks = (torch.squeeze(self.weights) > self.mask_epsilon).float()
+        starts = torch.argmax(masks, dim=1)  # returns *first* maximum
+        for ind in range(self.n_kernel):
+            masks[ind, int(starts[ind].item()):] = 1
+        return masks
